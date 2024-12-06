73a3935c6214fd5a971e320e515e8c39757f326d
klovelace21 <kennethlovelace21@gmail.com>
2024-12-06 15:46:44 -0500

    Abstract GamePiece created

---
+++ Classes/Abstract Classes/GamePiece.h
@@ -0,0 +1,22 @@
+//
+// Created by kenny on 12/6/24.
+//
+
+#ifndef GAMEPIECE_H
+#define GAMEPIECE_H
+
+
+
+class GamePiece {
+protected:
+  int row = 0;
+  int column = 0;
+public:
+
+  virtual ~GamePiece() = default;
+
+};
+
+
+
+#endif //GAMEPIECE_H
