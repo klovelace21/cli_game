7ff110fa5886c5ee5decf340628b12d6663a8c74
klovelace21 <kennethlovelace21@gmail.com>
2024-12-07 09:45:15 -0500

    Character Class contains ability array

--- Classes/AbstractClasses/Character/Character.h
+++ Classes/AbstractClasses/Character/Character.h
@@ -6,7 +6,7 @@
 #define CHARACTER_H
 
 #include "../GamePiece/GamePiece.h"
-
+#include "../../Ability/Ability.h"
 class Character : public GamePiece {
 protected:
   int maxHealthPoints;
@@ -17,8 +17,10 @@ public:
   Character(const std::string &_name, int _maxHealthPoints);
   Character(int _row, int _column, const std::string &_name, int _maxHealthPoints);
   int getCurrentHealthPoints() const;
+  Ability abilities[4];
   virtual void takeDamage(int _damage) = 0;
   virtual void restoreHealthPoints(int _toRestore) = 0;
+
 };
 
 
--- Classes/Player/Player.h
+++ Classes/Player/Player.h
@@ -10,6 +10,7 @@ class Player : public Character {
 public:
   Player(const std::string &_name, int _maxHealthPoints);
   Player(int _row, int _column, const std::string &_name, int _maxHealthPoints);
+
   void takeDamage(int _damage) override;
   void restoreHealthPoints(int _toRestore) override;
   void changeRow(int rowChangeVal);
