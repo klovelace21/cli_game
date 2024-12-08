3d5eaa54b72a66d5209d147b1615fa80d9a164b9
klovelace21 <kennethlovelace21@gmail.com>
2024-12-07 19:52:31 -0500

    AbilityBook init

---
+++ Classes/AbilityBook/AbilityBook.cpp
@@ -0,0 +1,10 @@
+//
+// Created by kenny on 12/7/24.
+//
+
+#include "AbilityBook.h"
+std::string craftName(Ability* toLearn) {
+  return "Book of " + toLearn->getName();
+}
+AbilityBook::AbilityBook(int _row, int _column, Ability* _toLearn)
+    : Item(_row, _column, craftName(_toLearn)), toLearn(_toLearn) {}
---
+++ Classes/AbilityBook/AbilityBook.h
@@ -0,0 +1,20 @@
+//
+// Created by kenny on 12/7/24.
+//
+
+#ifndef ABILITYBOOK_H
+#define ABILITYBOOK_H
+
+#include "../AbstractClasses/Item/Item.h"
+#include "../Ability/Ability.h"
+
+class AbilityBook : public Item {
+private:
+  Ability* toLearn;
+public:
+  AbilityBook(int _row, int _column, Ability* _toLearn);
+};
+
+
+
+#endif //ABILITYBOOK_H
