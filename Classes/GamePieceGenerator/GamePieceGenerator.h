eee69fd7debfe6cbee4dd3f4d5f6eebef7c2fd49
klovelace21 <kennethlovelace21@gmail.com>
2024-12-10 14:55:55 -0500

    GamePieceGenerator init

---
+++ Classes/GamePieceGenerator/GamePieceGenerator.h
@@ -0,0 +1,18 @@
+//
+// Created by kenny on 12/10/24.
+//
+
+#ifndef GAMEPIECEGENERATOR_H
+#define GAMEPIECEGENERATOR_H
+#include <string>
+#include "../AbstractClasses/GamePiece/GamePiece.h"
+class GamePieceGenerator {
+  const static std::string names[30];
+  const static std::string epithets[30];
+public:
+  GamePieceGenerator();
+};
+
+
+
+#endif //GAMEPIECEGENERATOR_H
