2fdc77de0d1a8c18fdbdb5999aadf76e67e00d7f
klovelace21 <kennethlovelace21@gmail.com>
2024-12-11 16:44:16 -0500

    Generates random ability with constructor

--- Classes/Ability/Ability.cpp
+++ Classes/Ability/Ability.cpp
@@ -3,14 +3,42 @@
 //
 
 #include "Ability.h"
-Ability::Ability() : name("Basic Attack"), damage(10) {}
+
+Ability::Ability() : name("Basic Attack"), effectAmount(10) {}
+
 Ability::Ability(const std::string &_name, int _damage)
-    : name(_name), damage(_damage) {}
+    : name(_name), effectAmount(_damage) {}
+
+Ability::Ability(Globals::Type _type)
+    : name(abilityPool.at(_type).at(rand() % 20)), effectAmount(5 * (rand() % 5)){}
+
 
 std::string Ability::getName() const { return name; }
 
-int Ability::getDamage() const { return damage; }
+int Ability::getEffectAmount() const { return effectAmount; }
 
+// change later
 std::string Ability::toString() const {
-    return getName() + ": " + std::to_string(getDamage()) + " damage";
+    return getName() + ": " + std::to_string(getEffectAmount()) + " effectAmount";
 }
+map<Globals::Type, vector<string>> Ability::abilityPool = {
+    // DAMAGE
+    {Globals::DAMAGE, { "Infernal Surge", "Crimson Cascade", "Soul Rend", "Scorching Arc", "Shadow Nova",
+          "Razor Tempest", "Eclipse Strike", "Hellfire Barrage", "Thunderclap Smash", "Bladed Cyclone",
+          "Venomous Spikes", "Starfall Impact", "Volcanic Blast", "Frozen Lance", "Phantom Shard",
+          "Chaos Wave", "Obsidian Breaker", "Bloodletting Swipe", "Electric Onslaught", "Meteor Crash" }
+    },
+// HEALING
+{Globals::HEALING, { "Healing Light", "Restorative Touch", "Renewing Wave", "Life Blossom", "Vital Surge",
+"Soothing Breeze", "Rejuvenation Pulse", "Divine Mend", "Aura of Renewal", "Sanctuary Shield",
+"Graceful Restoration", "Cleansing Waters", "Radiant Embrace", "Life's Harmony", "Celestial Balm",
+"Sacred Rebirth", "Essence of Vitality", "Healing Rain", "Light of Hope", "Tranquility Bloom" }
+},
+    // BUFF
+{Globals::BUFF,{ "Strength of Titans", "Ethereal Grace", "Empowering Roar", "Shield of Valor", "Blazing Resolve",
+  "Arcane Amplification", "Blessing of Fortitude", "Wrathful Ascension", "Guardian's Will", "Spiritual Uplift",
+  "Fury of the Storm", "Celestial Blessing", "Mighty Surge", "Unyielding Spirit", "Wings of the Phoenix",
+  "Hero's Will", "Battle Hymn", "Radiance of the Ancients", "Unstoppable Force", "Empowerment Aura" }
+}
+};
+// change to array
--- Classes/Ability/Ability.h
+++ Classes/Ability/Ability.h
@@ -4,20 +4,26 @@
 
 #ifndef ABILITY_H
 #define ABILITY_H
+#include <map>
 #include <string>
-
-
+#include <vector>
+#include <iostream>
+#include "../../Globals/Globals.h"
+using namespace std;
 class Ability {
 private:
+
   std::string name;
-  int damage;
+  int effectAmount;
 public:
+  static map<Globals::Type, vector<string>> abilityPool;
   Ability();
   Ability(const std::string &_name, int _damage);
+  Ability(Globals::Type _type);
 
   std::string getName() const;
   std::string toString() const;
-  int getDamage() const;
+  int getEffectAmount() const;
 
 };
 
