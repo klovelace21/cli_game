f15555e6eb408dee3f6af1a757e2211c548f1047
klovelace21 <kennethlovelace21@gmail.com>
2024-12-11 15:10:35 -0500

    Remove row / col from GamePiece

--- Classes/AbstractClasses/GamePiece/GamePiece.cpp
+++ Classes/AbstractClasses/GamePiece/GamePiece.cpp
@@ -3,14 +3,9 @@
 //
 #include "GamePiece.h"
 
-GamePiece::GamePiece() : row(), column() {}
-GamePiece::GamePiece(int _row, int _column, const std::string &_name)
-    : row(_row), column(_column), name(_name) {}
 
-GamePiece::GamePiece(const std::string &_name) : name(_name) {}
+GamePiece::GamePiece(const std::string &_name)
+    : name(_name) {}
 
-int GamePiece::getRow() const { return row; }
-
-int GamePiece::getColumn() const { return column; }
 
 std::string GamePiece::getName() const { return name; }
--- Classes/AbstractClasses/GamePiece/GamePiece.h
+++ Classes/AbstractClasses/GamePiece/GamePiece.h
@@ -8,19 +8,14 @@
 
 class GamePiece {
 protected:
-    int row;
-    int column;
     std::string name;
 
 public:
     virtual ~GamePiece() = default;
 
     GamePiece();
-    GamePiece(int _row, int _column, const std::string &_name);
-    explicit GamePiece(const std::string &_name);
+    GamePiece(const std::string &_name);
 
-    int getRow() const;
-    int getColumn() const;
     std::string getName() const;
 };
 
