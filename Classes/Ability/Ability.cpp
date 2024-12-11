a0500d8f585b414969372688b793907850ccfb38
klovelace21 <kennethlovelace21@gmail.com>
2024-12-11 16:49:22 -0500

    Add Ability Type to Ability

--- Classes/Ability/Ability.cpp
+++ Classes/Ability/Ability.cpp
@@ -4,23 +4,27 @@
 
 #include "Ability.h"
 
-Ability::Ability() : name("Basic Attack"), effectAmount(10) {}
+Ability::Ability() : name("Basic Attack"), effectAmount(10), type(Globals::Type::DAMAGE) {}
 
-Ability::Ability(const std::string &_name, int _damage)
-    : name(_name), effectAmount(_damage) {}
+Ability::Ability(const Globals::Type _type, const std::string &_name, int _effectAmount)
+    : name(_name), effectAmount(_effectAmount), type(_type) {}
 
 Ability::Ability(Globals::Type _type)
-    : name(abilityPool.at(_type).at(rand() % 20)), effectAmount(5 * (rand() % 5)){}
+    : name(abilityPool.at(_type).at(rand() % 20)), effectAmount(5 * (rand() % 5)), type(_type) {}
 
 
 std::string Ability::getName() const { return name; }
 
 int Ability::getEffectAmount() const { return effectAmount; }
 
+Globals::Type Ability::getType() const { return type; }
+
 // change later
 std::string Ability::toString() const {
     return getName() + ": " + std::to_string(getEffectAmount()) + " effectAmount";
 }
+
+
 map<Globals::Type, vector<string>> Ability::abilityPool = {
     // DAMAGE
     {Globals::DAMAGE, { "Infernal Surge", "Crimson Cascade", "Soul Rend", "Scorching Arc", "Shadow Nova",
--- Classes/Ability/Ability.h
+++ Classes/Ability/Ability.h
@@ -12,17 +12,18 @@
 using namespace std;
 class Ability {
 private:
-
+  const Globals::Type type;
   std::string name;
   int effectAmount;
 public:
   static map<Globals::Type, vector<string>> abilityPool;
   Ability();
-  Ability(const std::string &_name, int _damage);
+  Ability(Globals::Type _type, const std::string &_name, int _damage);
   Ability(Globals::Type _type);
 
   std::string getName() const;
   std::string toString() const;
+  Globals::Type getType() const;
   int getEffectAmount() const;
 
 };
