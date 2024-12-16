e119386dc02eed187e9b91a5bc50efbb1c8ca560
klovelace21 <kennethlovelace21@gmail.com>
2024-12-16 10:43:30 -0500

    genRandomType global

--- Globals/Globals.cpp
+++ Globals/Globals.cpp
@@ -7,4 +7,16 @@
 namespace Globals {
     int BOARD_HEIGHT = 10;
     int BOARD_WIDTH = 10;
+    // Change to accommodate buff 2 -> 3
+    Type genRandomType() {
+        int typeNum = rand() % 2;
+        Type type;
+        switch (typeNum) {
+            case 0:
+                type = Type::DAMAGE; break;
+            case 1:
+                type = Type::HEALING; break;
+        }
+        return type;
+    }
 }
--- Globals/Globals.h
+++ Globals/Globals.h
@@ -4,13 +4,14 @@
 
 #ifndef GLOBALS_H
 #define GLOBALS_H
-
+#include <cstdlib>
 
 
 namespace Globals {
     extern int BOARD_HEIGHT;
     extern int BOARD_WIDTH;
     enum Type { DAMAGE = 0, HEALING = 1, BUFF = 2};
+    extern Type genRandomType();
 };
 
 
